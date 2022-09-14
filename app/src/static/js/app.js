20a2eddf93fe06ac3e869da0d11b8cfab732cfd7
alexosousa <alex.sousa@totalnuvem.com>
2022-09-14 07:24:53 -0300

    v.0

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">Sem itens! Adicione um novo agora!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
